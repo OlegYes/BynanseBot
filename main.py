ba20ac75e580236731aaf97de42f1a13745a6387
OlegYesikov <o.yesikov@gmail.com>
2022-08-21 18:25:43 +0200

    fixs errors

--- main.py
+++ main.py
@@ -25,25 +25,39 @@ def price(symbol):
 # the function sells all available coins of a certain symbol in our case "DREP"
 def sell_coin():
     balance = client.get_asset_balance(asset='DREP') # ask binance for information about the balance of the coin
-    all_coin_balanse = balance['free']  #  select the amount of free coins from our balance, "free" are those we can work with and are not scheduled in other orders(see the documentation " get_asset_balance" https://python-binance.readthedocs.io/en/latest/binance.html?highlight=client.get_asset_balance#binance.client.Client.get_asset_balance)
-    print("Sell")
-    order = client.order_market_sell(  # we create an order at the market price
-        symbol='DREPUSDT',  # the currency pair on which you want to create an order
-        quantity=all_coin_balanse)  # the number of coins we want to sell, in our case we selected all the coins of this pair
-    return 0
+    all_coin_balanse = float(balance['free'])  #  select the amount of free coins from our balance, "free" are those we can work with and are not scheduled in other orders(see the documentation " get_asset_balance" https://python-binance.readthedocs.io/en/latest/binance.html?highlight=client.get_asset_balance#binance.client.Client.get_asset_balance)
+
+    if all_coin_balanse > 20:
+        print("Sell")
+        order = client.order_market_sell(  # we create an order at the market price
+            symbol='DREPUSDT',  # the currency pair on which you want to create an order
+            quantity=all_coin_balanse)  # the number of coins we want to sell, in our case we selected all the coins of this pair
+
 
 # the function buys coins for the entire balance of the wallet
 def buy_coin():   # функція для покупки понеток, купуємо на всі гроші які у нас є
     balance_usdt = client.get_asset_balance(asset='USDT')
     all_coin_balanse_usdt = float(balance_usdt['free']) # get the amount of USDT contained in our cart
-    a = price('DREPUSDT') # here we lose the price(symbol) function described above and get the price of DREP vs. USDT
-    coin_byers = all_coin_balanse_usdt // a #  by a simple formula we calculate how many coins we can buy
-    print("Buy")
-    order = client.order_market_buy( # we create a purchase order
-        symbol='DREPUSDT',
-        quantity=coin_byers)
-    return 0
+    if all_coin_balanse_usdt >= 10:
+        a = price('DREPUSDT') # here we lose the price(symbol) function described above and get the price of DREP vs. USDT
+        coin_byers = all_coin_balanse_usdt // a #  by a simple formula we calculate how many coins we can buy
+        print("Buy")
+        order = client.order_market_buy( # we create a purchase order
+            symbol='DREPUSDT',
+            quantity=coin_byers)
 
+def parse(body):
+    soup = BeautifulSoup(body, 'lxml')
+    # soups = soup.find_all("td", valign="top", align="left", style="text-align: left; padding: 0px 0px;")
+    soups = soup.find("p",
+                      style="font-family: -apple-system, BlinkMacSystemFont, 'Trebuchet MS', Roboto, Ubuntu, sans-serif;font-size: 18px;line-height: 28px;margin: 0;padding: 0;text-align: center !important;white-space: pre-line;color: #131722;")
+    # print(soups.text)
+
+    with open("test.json", "w", encoding='utf-8') as w:
+        w.write(soups.text)
+    with open("test.json", "r") as read_file:
+        dataInfo = json.load(read_file)
+    return dataInfo
 
 # a function that implements receiving information and reading the message that we received from the Tradingview website
 def get_messege():
@@ -82,54 +96,61 @@ def get_messege():
             for payload in email_message.get_payload():
                 # print(payload)
                 body = payload.get_payload(decode=True).decode('utf-8')
-                #print(body)
+                # print(body)
+                dataInfo = parse(body)
+                return dataInfo
         else:
             body = email_message.get_payload(decode=True).decode('utf-8')
-            #print(body)
-
-            soup = BeautifulSoup(body, 'lxml')
-            #soups = soup.find_all("td", valign="top", align="left", style="text-align: left; padding: 0px 0px;")
-            soups = soup.find("p", style="font-family: -apple-system, BlinkMacSystemFont, 'Trebuchet MS', Roboto, Ubuntu, sans-serif;font-size: 18px;line-height: 28px;margin: 0;padding: 0;text-align: center !important;white-space: pre-line;color: #131722;")
-            #print(soups.text)
-
-            with open("test.json", "w", encoding='utf-8') as w:
-                w.write(soups.text)
-            with open("test.json", "r") as read_file:
-                dataInfo = json.load(read_file)
+            # print(body)
+            dataInfo = parse(body)
             return dataInfo
 
 
+
 dTime = "2022-08-20T00:15:00Z"
+price_order_duy = price("DREPUSDT")
 def logic():   #  the logic of our bot, according to which and when we will buy/sell on the market
+
     messege_mail = get_messege()
     count = False
-    global dTime
-    if messege_mail['dTime'] != dTime:  # we filter the new message from the old one so that the system does not take the previous message again
-        dTime = messege_mail['dTime']
-        balance_main = client.get_asset_balance(asset='DREP')
-        all_coin_balanse_main = balance_main['free'] # see if we have coins
-        if messege_mail['do_it'] == 'sell ':  # we read what is done on the market, people sell (sell)
-            if all_coin_balanse_main == 0:  # if there are no coins - we buy them
-                count = True
-                buy_coin()
-                price_order_duy = price("DREPUSDT") # fix the price at which we bought it
-        elif messege_mail['do_it'] == 'buy ':  # if people buy we will sell
-            if all_coin_balanse_main != 0:
-                sell_coin()
+    global dTime, price_order_duy
+    if messege_mail != None:
 
-    if count == True:  #   this block of code will sell coins if their price changes by 1 percent in any direction, I decided so based on my risk management
-        proc = price_order_duy/100
-        prise_real_time = price("DREPUSDT")
-        delta = abs(prise_real_time - price_order_duy)
-        delta_proch = delta/proc
-        if delta_proch >= 1:
-            sell_coin()
+        if messege_mail['dTime'] != dTime:  # we filter the new message from the old one so that the system does not take the previous message again
+            print("***START***")
+            dTime = messege_mail['dTime']
+            print(dTime)
+            balance_main = client.get_asset_balance(asset='DREP')
+            all_coin_balanse_main = float(balance_main['free']) # see if we have coins
+            print('DREP = ', all_coin_balanse_main)
+            print("do_it: ", messege_mail['do_it'])
+            if messege_mail['do_it'] == 'sell ':  # we read what is done on the market, people sell (sell)
+                if all_coin_balanse_main <= 20:  # if there are no coins - we buy them
+                    buy_coin()
+                    price_order_duy = price("DREPUSDT") # fix the price at which we bought it
+                    print(price_order_duy)
+                    count = True
+            if messege_mail['do_it'] == 'buy ':  # if people buy we will sell
+                if all_coin_balanse_main != 0:
+                    sell_coin()
+                    count = True
+            print("***END***")
+
+        if count == True:  #   this block of code will sell coins if their price changes by 1 percent in any direction, I decided so based on my risk management
+            proc = price_order_duy/100
+            prise_real_time = price("DREPUSDT")
+            delta = abs(prise_real_time - price_order_duy)
+            delta_proch = delta/proc
+            if delta_proch >= 1:
+                sell_coin()
+                print("******\n1% LIMITED\n******")
+
 
 
 
 def runner():   #   needed to restart the functions, we restart the script for reactivation
-  threading.Timer(60.0, runner).start()  # Restart after 60 seconds
-  logic()
+    threading.Timer(30.0, runner).start()  # Restart after 30 seconds
+    logic()
 
 
 if __name__ == '__main__':
